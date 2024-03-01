ace73f7af3f8ac4d04c7ae2593342facd0bf7599
Yanone <post@yanone.de>
2024-03-01 14:27:39 +0100

    Simpler command after gftools update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 gftools builder sources/config.yaml
 gftools-rename-font -o fonts/ttf/SedanSC-Regular.ttf fonts/ttf/Sedan-Regular.ttf "Sedan SC"
-gftools-remap-layout fonts/ttf/SedanSC-Regular.ttf "DFLT/dflt/smcp -> DFLT/dflt/ccmp" "latn/dflt/smcp -> latn/dflt/ccmp"
+gftools-remap-layout fonts/ttf/SedanSC-Regular.ttf "smcp => ccmp"
